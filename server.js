d82c030365413faef739387e1868e70b16da9289
Jeff Bell <jeffmbell@gmail.com>
2016-05-13 13:39:25 -0400

    Add DELETE todos route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var _ = require('underscrore');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -46,6 +46,19 @@ app.post('/todos', function(req, res) {
   todos.push(body);
   
   res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('todos/:id', function(req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+  
+  if (matchedTodo) {
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  } else {
+    res.status(404).json({"error": "no todo found with that id"});
+  }
 });
 
 app.listen(PORT, function() {
